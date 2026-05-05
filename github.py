43bbd117c1e9bae4899615b14734542f8676b4d8
ege3506 <mertege2001@gmail.com>
2026-05-05 19:22:56 +0300

    Add files via upload

---
+++ github.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import math 
+x= 5.2
+def round_up(x):
+    
+    return print(math.ceil(x))
+
+def round_down(x):
+    return print(math.floor(x))
+
+    round_down(x)
+    round_up(x)
+    
+
+
+
+
